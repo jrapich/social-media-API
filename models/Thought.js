6b40f6632cbd809d437bc70f401e4110c68fe3a2
jeremysr <jeremysr@protonmail.com>
2023-10-21 17:32:08 -0600

    added devlogs, added dayjs to format date, added getter to format date on each query

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,13 @@
 const { Schema, model } = require('mongoose');
+//dayjs for formatting dates
+const dayjs = require('dayjs');
+//check the devlog env variable to see if we are logging extra things for dev purposes
+const devLog = process.env.DEVLOGGING === 'true' ? true : false;
+
+//function to format dates we will use in a getter
+function formatDate (date){
+    return dayjs(date).format('MM/DD/YYYY:hh:mm:ss:A');
+}
 
 // Schema to create Thought model
 const thoughtSchema = new Schema(
@@ -14,7 +23,9 @@ const thoughtSchema = new Schema(
         //thought created timestamp. use dayJS to format with a getter method 
         createdAt: {
             Type:Date,
-
+            default: Date.now,
+            get: formatDate,
+            
         },
         //the user who created the thought
         username: {
@@ -23,6 +34,9 @@ const thoughtSchema = new Schema(
         },
     },
     {
+        //if we added this, mongoose would add createdAt and updatedAt by default automatically whenever the doc is created or updated
+        //we aren't using this though because we are using a manual getter for createdAt and timestamps:true will overwrite anything we use for that property
+        //timestamps:true,
         toJSON: {
           virtuals: true,
           //here we add getters so the get property on createdAt will function every time this model is queried
@@ -35,5 +49,10 @@ const thoughtSchema = new Schema(
 
 // Initialize our THOUGHT model
 const Thought = model('thought', thoughtSchema);
+
+//devlogs for testing
+if (devLog) {
+    console.log(formatDate(Date.now));
+}
 
 module.exports = Thought;
