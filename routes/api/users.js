92b48f7c29f19f44008da86685699dcfc237ffc3
jeremysr <jeremysr@protonmail.com>
2023-10-21 18:30:25 -0600

    added route for api/users, pseudocode for users routes

---
+++ routes/api/users.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+//GET route for returning all users
+
+//GET route for returning single user by id
+
+//POST route for for creating a new user, no need for auth
+
+//PUT route to update current user by _id
+
+//DELETE route to delete a user by _id
+
+module.exports = router;
