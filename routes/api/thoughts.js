606ae98399013af188bede8310ddd7651fcad94e
jeremysr <jeremysr@protonmail.com>
2023-10-21 19:17:23 -0600

    added thoughts/reaction routes

---
+++ routes/api/thoughts.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const {allThoughts, singleThought, createThought, updateThought, deleteThought, addReaction, deleteReaction} = require('../../controllers/thoughts.js');
+
+//GET route to return all thoughts
+router.get('/all', allThoughts);
+//GET route for a single thought
+router.get('/:id', singleThought);
+//POST route to create a thought
+router.post('/create', createThought);
+//PUT to update a thought
+router.put('/update/:id', updateThought);
+//delete a thought
+router.delete('/delete/:id', deleteThought);
+//add reaction to a thought
+router.post('/:id/reactions/add', addReaction);
+//delete a reation from a thought
+router.delete('/:id/reactions/delete/:reactionID');
+
+module.exports = router;
