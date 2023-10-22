d1f4a0f3512ee4e6f85f552c5894f2c30aa352ce
jeremysr <jeremysr@protonmail.com>
2023-10-21 19:08:04 -0600

    added routes for managing friends

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,19 @@
 const router = require('express').Router();
-const {getUsers, getSingleUser, createuser, updateUser, deleteUser} = require('../../controllers/users');
+const {getUsers, getSingleUser, createUser, updateUser, deleteUser} = require('../../controllers/users.js');
+const {addFriend, deleteFriend} = require('../../controllers/friends.js');
 
 //GET route for returning all users
 router.get('/all', getUsers);
 //GET route for returning single user by id
 router.get('/:id', getSingleUser);
 //POST route for for creating a new user, no need for auth
-router.post('/create', createuser);
+router.post('/create', createUser);
 //PUT route to update current user by _id
 router.put('/update/:id', updateUser);
 //DELETE route to delete a user by _id
 router.delete('/delete/:id', deleteUser);
+//POST route to add a new friend to the user's friends list
+router.post('/:userID/friends/:friendID', addFriend);
+//DELETE route to delete a current friend from friend list
+router.delete('/:userID/friends/:friendID', deleteFriend);
 module.exports = router;
