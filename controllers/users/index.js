863277426d08adb3c00ce160c3c7200602720c06
jeremysr <jeremysr@protonmail.com>
2023-10-21 18:40:44 -0600

    added file structure for users controllers

---
+++ controllers/users/index.js
@@ -0,0 +1,7 @@
+const createUser = require('./createUser');
+const getUsers = require('./getUsers');
+const getSingleUser = require('./getSingleUser'); 
+const updateUser = require('./updateUser');
+const deleteUser = require('./deleteUser');
+
+module.exports = {createUser, getUsers, getSingleUser, createUser, updateUser, deleteUser};
